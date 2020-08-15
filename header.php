fc5c8a446328ea3de0b6ad19e62bcc481459d997
Diego Camino <diego.camino.r@gmail.com>
2020-08-15 16:55:41 -0500

    First theme created, add libraries and js scripts

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head() ?>
+  <title>Document</title>
+</head>
+<body>
+
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" alt="logo">
+      </div>
+    </div>
+  </div>
+</header>
