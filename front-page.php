1d0d3cf4aa30553262d41359cfd286459a021359
Diego Camino <diego.camino.r@gmail.com>
2020-08-16 00:42:58 -0500

    Custom loop for products

--- front-page.php
+++ front-page.php
@@ -9,6 +9,43 @@
 
         <?php    }
     }?>
+
+    <div class="lista-productos my-5">
+        <h2 class='text-center'>PRODUCTOS</h2>
+        <div class="row">
+        <?php
+        $args = array(
+            'post_type' => 'producto',
+            'post_per_page' => -1,
+            'order'         => 'ASC',
+            'orderby'       => 'title'
+        );
+
+        $productos = new WP_Query($args);
+
+        if($productos->have_posts()){
+            while($productos->have_posts()){
+                $productos->the_post();
+                ?>
+
+                <div class="col-4">
+                  <a href="<?php the_permalink(); ?>">
+                    <figure>
+                        <?php the_post_thumbnail('large'); ?>
+                    </figure>
+                    <h4 class='my-3 text-center'>
+                      <?php the_title();?>
+                    </h4>
+                  </a>
+
+                </div>
+
+           <?php }
+        }
+
+        ?>
+      </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
